ac08fcbf0d978a19f783fed5b2e3bf1e41c33552
marcus604 <2810211+marcus604@users.noreply.github.com>
2024-07-07 01:21:02 -0700

    feat: automate pushing new local changes

---
+++ push_aliases.sh
@@ -0,0 +1,15 @@
+read -p "Enter branch name: " branch_name
+read -p "Enter commit message: " commit_msg
+
+cp ~/.aliases ~/dev/Shell/aliases
+
+cd ~/dev/Shell
+
+git pull
+git add aliases
+
+git checkout -b "$branch_name"
+
+git commit -m "$commit_msg"
+
+git push origin
