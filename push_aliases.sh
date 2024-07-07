8bdae558f0e647d95d2bf7e37d4a0e1c6828c690
marcus604 <2810211+marcus604@users.noreply.github.com>
2024-07-07 02:06:46 -0700

    fix: pull before making file change (#4)

--- push_aliases.sh
+++ push_aliases.sh
@@ -1,11 +1,12 @@
 read -p "Enter branch name: " branch_name
 read -p "Enter commit message: " commit_msg
 
+git pull
+
 cp ~/.aliases ~/dev/Shell/aliases
 
 cd ~/dev/Shell
 
-git pull
 git add aliases
 
 git checkout -b "$branch_name"
